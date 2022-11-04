7da441b03275415261a1bd233eb652550f52033d
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-11-04 07:20:43 -0500

    Add project files.

---
+++ EvilClone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptainAmazing
+{
+    internal class EvilClone
+    {
+        public static int CloneCount = 0;
+        public int CloneID { get; } = ++CloneCount;
+
+        public EvilClone() => Console.WriteLine($"Clone #{CloneID} is wreaking havoc");
+        ~EvilClone()
+        {
+            Console.WriteLine($"Clone #{CloneID} destroyed");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CaptainAmazing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            var clones = new List<EvilClone>();
+            while (true)
+            {
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'a':
+                        clones.Add(new EvilClone());
+                        break;
+                    case 'c':
+                        Console.WriteLine($"Clearing list at time {stopwatch.ElapsedMilliseconds}");
+                        clones.Clear();
+                        break;
+                    case 'g':
+                        Console.WriteLine($"Collecting at time {stopwatch.ElapsedMilliseconds}");
+                        GC.Collect();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
